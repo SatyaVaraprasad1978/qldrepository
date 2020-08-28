8b93d29ef1a02856d4ac49c3d454a983b1976854
SatyaVaraprasad1978 <36626021+SatyaVaraprasad1978@users.noreply.github.com>
2020-08-28 19:03:55 +0530

    Update aibscript.ps1

--- AzureImagebuilder/NewFolder/aibscript.ps1
+++ AzureImagebuilder/NewFolder/aibscript.ps1
@@ -16,7 +16,7 @@ New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
 #Invoke-WebRequest -Uri 'https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.x64.exe' -OutFile 'c:\temp\notepadplusplus.exe'
 #Invoke-Expression -Command 'c:\temp\notepadplusplus.exe /S'
 
-#Start sleep
+Start sleep
 Start-Sleep -Seconds 10
 
 #InstallFSLogix
